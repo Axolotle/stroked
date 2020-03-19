1170d111edf47c5a06413aad4815f056d0c40fe1
Axolotle <nchesnais@mailoo.org>
2020-03-19 16:32:45 +0100

    add uniform transformation

--- stroked/ui/canvas.py
+++ stroked/ui/canvas.py
@@ -14,39 +14,38 @@ class Canvas(Gtk.DrawingArea):
         self.path = []
         self.grid = (5, 9)
         self.margin = (1, 1)
+        self.size = (self.grid[0] + self.margin[0], self.grid[1] + self.margin[1])
 
-        self.scale = None
+        self.scale = 0.01
+        self.zoom = 1
+
+        self.origin = (0, 0)
+        self.drag = (0, 0)
+
         self.linewidth = 1
         self.linecap = 1
         self.linejoin = 1
 
-        self.translation = {
-            'origin': (1, 1),
-            'start': (0, 0),
-            'current': (0, 0)
-        }
-
-        self.connect('draw', self.draw)
         self.connect('button-press-event', self.on_mouse_press)
         self.connect('motion-notify-event', self.on_mouse_move)
         self.connect('button-release-event', self.on_mouse_release)
+        self.connect('scroll-event', self.on_scroll)
         self.connect('size-allocate', self.on_resize)
+        self.connect('draw', self.draw)
         self.set_events(self.get_events() |
             Gdk.EventMask.BUTTON_PRESS_MASK |
             Gdk.EventMask.POINTER_MOTION_MASK |
-            Gdk.EventMask.BUTTON_RELEASE_MASK)
+            Gdk.EventMask.BUTTON_RELEASE_MASK |
+            Gdk.EventMask.SCROLL_MASK)
 
     def draw(self, widget, ctx):
-        scale = 1 / self.scale
-
         ctx.set_source_rgba(0.1, 0.1, 0.1, 1)
         ctx.paint()
 
-        ori = self.translation['origin']
-        start = self.translation['start']
-        cur = self.translation['current']
-        ctx.translate(ori[0] + (cur[0] - start[0]), ori[1] + (cur[1] - start[1]))
-        ctx.scale(scale, scale)
+        ori = self.origin
+        ctx.translate(ori[0] - (self.size[0] / 2 / self.scale * self.zoom),
+                      ori[1] - (self.size[1] / 2 / self.scale * self.zoom))
+        ctx.scale(1 / self.scale * self.zoom, 1 / self.scale * self.zoom)
 
         self.draw_grid(ctx)
 
@@ -58,25 +57,33 @@ class Canvas(Gtk.DrawingArea):
         ctx.new_path()
         if len(self.path) == 1:
             x, y = self.path[0]
-            ctx.line_to(x, y)
-            ctx.line_to(x, y)
+            ctx.line_to(x + self.margin[0], y + self.margin[1])
+            ctx.line_to(x + self.margin[0], y + self.margin[1])
         else:
             for x, y in self.path:
-                ctx.line_to(x, y)
+                ctx.line_to(x + self.margin[0], y + self.margin[1])
+
         ctx.stroke()
 
     def draw_grid(self, ctx):
         ctx.set_source_rgb(0.13, 0.3, 0.89)
         for x in range(self.grid[0]):
             for y in range(self.grid[1]):
-                ctx.arc(x, y, self.scale * 5, 0.0, 2 * pi)
+                ctx.arc(x + self.margin[0],
+                        y + self.margin[1],
+                        self.scale * 5 / self.zoom,
+                        0.0,
+                        2 * pi)
                 ctx.fill()
 
     def on_mouse_move(self, widget, event):
         if event.state & Gdk.EventMask.BUTTON_PRESS_MASK:
             self.add_point(event.x, event.y)
         if event.state & Gdk.ModifierType.BUTTON2_MASK:
-            self.translation['current'] = (event.x, event.y)
+            ori = self.origin
+            prev = self.drag
+            self.origin = (ori[0] + (event.x - prev[0]), ori[1] + (event.y - prev[1]))
+            self.drag = (event.x, event.y)
             self.queue_draw()
 
     def on_mouse_press(self, widget, event):
@@ -86,30 +93,38 @@ class Canvas(Gtk.DrawingArea):
             self.path = []
             self.queue_draw()
         elif event.button == Gdk.BUTTON_MIDDLE:
-            self.translation['start'] = (event.x, event.y)
-            self.translation['current'] = (event.x, event.y)
+            self.drag = (event.x, event.y)
 
     def on_mouse_release(self, widget, event):
         if event.button == Gdk.BUTTON_MIDDLE:
-            ori = self.translation['origin']
-            start = self.translation['start']
-            cur = self.translation['current']
-            self.translation['origin'] = (ori[0] + (cur[0] - start[0]), ori[1] + (cur[1] - start[1]))
-            start = self.translation['start'] = (0, 0)
-            cur = self.translation['current'] = (0, 0)
+            ori = self.origin
+            prev = self.drag
+            self.origin = (ori[0] + (event.x - prev[0]), ori[1] + (event.y - prev[1]))
+            self.drag = (0, 0)
             self.queue_draw()
 
+    def on_scroll(self, widget, event):
+        if event.direction == Gdk.ScrollDirection.UP:
+            self.zoom += 0.1
+        else:
+            self.zoom += -0.1
+        if self.zoom < 0.1:
+            self.zoom = 0.1
+        self.queue_draw()
+
     def on_resize(self, widget, rect):
+        self.origin = (rect.width / 2, rect.height / 2)
         self.scale = (self.grid[1] + self.margin[1]) / rect.height
-        factor = 1 / self.scale
-        self.translation['origin'] = (
-            self.margin[0] * factor, self.margin[1] * factor
-        )
-        self.set_size_request((self.grid[0] + self.margin[0]) * factor, -1)
 
     def add_point(self, x, y):
-        translate = self.translation['origin']
-        point = (round((x-translate[0]) * self.scale), round((y-translate[1]) * self.scale))
+        ori = self.origin
+        translate = (ori[0] - (self.size[0] / 2 / self.scale * self.zoom),
+                     ori[1] - (self.size[1] / 2 / self.scale * self.zoom))
+        point = (
+            round((x-translate[0]) * self.scale / self.zoom)-self.margin[0],
+            round((y-translate[1]) * self.scale / self.zoom)-self.margin[1]
+        )
+
         if len(self.path) == 0 or self.path[-1] != point:
             self.path.append(point)
             self.queue_draw()
