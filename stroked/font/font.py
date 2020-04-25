722390785990607bf8c43fab73c02cb798ec0374
Axolotle <nchesnais@mailoo.org>
2020-04-25 16:27:23 +0200

    add base font.info and font.lib value for new fonts

--- stroked/font/font.py
+++ stroked/font/font.py
@@ -4,8 +4,13 @@ from defcon import Font as DefFont
 
 
 class Font(DefFont):
-    def __init__(self):
-        super().__init__()
-
-        self.info.openTypeHeadCreated = '{:%Y/%m/%d %H:%M:%S}'.format(
-            date.today())
+    def __init__(self, path=None):
+        super().__init__(path=path)
+        if path is None:
+            self.info.openTypeHeadCreated = '{:%Y/%m/%d %H:%M:%S}'.format(
+                date.today())
+            self.info.versionMajor = 1
+            self.info.versionMinor = 0
+            self.info.openTypeNameVersion = 'Version: 1.000'
+            self.lib['gridWidth'] = 5
+            self.lib['gridHeight'] = 7
