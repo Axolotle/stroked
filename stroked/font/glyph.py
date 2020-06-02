d8c50a29880de8d5929f3872d9a75787920cef41
Axolotle <nchesnais@mailoo.org>
2020-06-02 15:33:01 +0200

    commenting methods of Glyph

--- stroked/font/glyph.py
+++ stroked/font/glyph.py
@@ -7,6 +7,9 @@ class Glyph(DefGlyph):
 
     @property
     def selected(self):
+        """
+        A boolean indicating if all contours are selected.
+        """
         for contour in self:
             if not contour.selected:
                 return False
@@ -14,11 +17,17 @@ class Glyph(DefGlyph):
 
     @selected.setter
     def selected(self, value):
+        """
+        Sets the selected attribute of all contours to **True** or **False**.
+        """
         for contour in self:
             contour.selected = value
 
     @property
     def selection(self):
+        """
+        Get the points of all contours that are selected as a `set`.
+        """
         selection = set()
         for contour in self:
             selection.update(contour.selection)
@@ -26,9 +35,18 @@ class Glyph(DefGlyph):
 
     @selection.setter
     def selection(self, selection):
+        """
+        Sets the selected attribute of all points of all contours to **True**
+        or **False** depending of the given selection.
+        """
         for contour in self:
             contour.selection = selection
 
     def move_selected(self, values):
+        """
+        Move the points of all contours that are selected by (x, y).
+
+        This posts a *Glyph.Changed* notification.
+        """
         for contour in self:
             contour.move_selected(values)
